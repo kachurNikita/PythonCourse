f6780fe85e277deeabacf49ac49d97fdef4412e4
Nikita <ashtonashtonkachur@gmail.com>
2022-10-20 08:51:35 -0700

    just first meeting with the python

---
+++ main.py
@@ -0,0 +1,54 @@
+#words = 'Hello \\test "World"'
+#print(words)
+
+
+verse = '''
+І день іде, і ніч іде.
+І голову схопивши в руки,
+Дивуєшся, чому не йде
+Апостол правди і науки!
+'''
+
+# \n - перенос строки
+
+#print(verse)
+
+#file = r'C:\d\new.txt'
+# r - rowString
+#print(file)
+
+#s = 'Py''thon'
+#print(s)
+
+#string1 = 'Hello, '
+#string2 = 'world'
+#string3 = string1 + string2
+#print(string3)
+
+
+#name = 'Ashton'
+#age = 27
+
+
+#print('My name is ' + name + ' i am ' + str(age) + ' years old')
+
+#Sequence of character's in strings (and strings indexations)
+
+stringSequence = 'Hello world'
+
+print(stringSequence[0])
+print(stringSequence[0:])
+print(stringSequence[6:])
+print(stringSequence[::2])
+print(stringSequence[::-1]) # string reverse
+print(stringSequence[0:4] +  stringSequence[5:])
+
+#print(stringSequence[-1])
+# String is immutable (we can't change string only recreate!)
+
+
+#SLICE - срез
+
+
+
+
